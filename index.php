361df9d7f533084d2c6a78da2dad432ae3652a3b
EdViTeR <maxpivnenko@mail.ru>
2024-10-14 15:38:57 +0300

    html

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-
-$a = 'Моя первая программа на PHP';
-echo $a;
+	$a = 'Моя первая программа на PHP';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная страница</title>
+</head>
+<body>
+	<?php echo $a; ?>
+</body>
+</html>
